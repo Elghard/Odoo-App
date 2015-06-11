567b477917695a2429d1b95f9c716c54cbefdd43
Andre Leander <leeshienwen@gmail.com>
2015-06-11 19:00:19 +0700

    Upload Changelog.rst. Version to 1.2

--- web_list_view_sticky/__openerp__.py
+++ web_list_view_sticky/__openerp__.py
@@ -1,13 +1,13 @@
 {
-    'name': 'List View Fixed Table Header',
-    'version': '1.1',
+    'name': 'Web List View Fixed Table Header',
+    'version': '1.2',
     'category': 'Tools',
     'sequence': 15,
-    'summary': 'List View Fixed Table Header',
+    'summary': 'Web List View Fixed Table Header',
     'description': """
-List View Fixed Table Header
+Web List View Fixed Table Header
 =================================
-* Fixed (sticky) list view table header, very helpful when dealing with many records.
+* Fixed (sticky) list view table header, very helpful when dealing with many record.
 """,
     'author': 'Andre Leander',
     'website': 'leeshienwen@gmail.com',
@@ -15,8 +15,6 @@ List View Fixed Table Header
     'data': [
         'views/web_list_view_sticky.xml',
     ],
-    'price': 2.0,
-    'currency': 'USD',
     'installable': True,
     'auto_install': False,
     'application': False,
