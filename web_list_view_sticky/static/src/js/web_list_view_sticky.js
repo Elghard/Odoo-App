18017a2f7f8d04969a6a34d34c0b4c21f43a6393
Andre Leander <Elghard@users.noreply.github.com>
2015-06-11 10:46:02 +0700

    Update LeftOffset to current parent left offset

--- web_list_view_sticky/static/src/js/web_list_view_sticky.js
+++ web_list_view_sticky/static/src/js/web_list_view_sticky.js
@@ -8,11 +8,12 @@ openerp.web_list_view_sticky = function (instance) {
         load_list: function () {
             var self = this;
             self._super.apply(this, arguments);
-            var offset = self.$el.parents('.oe_view_manager.oe_view_manager_current').find('.oe_view_manager_header').offset().top;
             var scrollArea = self.$el.parents('.oe_view_manager.oe_view_manager_current').find('.oe_view_manager_wrapper .oe_view_manager_body')[0];
-            self.$el.find('table.oe_list_content').each(function(){
-                $(this).stickyTableHeaders({scrollableArea: scrollArea})
-            });
+            if(scrollArea){
+                self.$el.find('table.oe_list_content').each(function(){
+                    $(this).stickyTableHeaders({scrollableArea: scrollArea, leftOffset: scrollArea})
+                });
+            }
         },
     });
 };
