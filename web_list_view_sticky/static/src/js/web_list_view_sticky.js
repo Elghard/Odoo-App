95bccf22f920448793c3887990802f771c8785ce
Deepak Kumar Dixit <deepak@ksolves.com>
2017-04-07 18:26:44 +0530

    Added support to Odoo 9

--- web_list_view_sticky/static/src/js/web_list_view_sticky.js
+++ web_list_view_sticky/static/src/js/web_list_view_sticky.js
@@ -1,19 +1,25 @@
-openerp.web_list_view_sticky = function (instance) {
-    var _t = instance.web._t,
-        _lt = instance.web._lt;
-    var QWeb = instance.web.qweb;
+odoo.define('web_list_view_sticky.ListView',function(require){
 
-    //Sticky Table Header
-    instance.web.ListView.include({
-        load_list: function () {
-            var self = this;
-            self._super.apply(this, arguments);
-            var scrollArea = self.$el.parents('.oe_view_manager.oe_view_manager_current').find('.oe_view_manager_wrapper .oe_view_manager_body')[0];
-            if(scrollArea){
-                self.$el.find('table.oe_list_content').each(function(){
-                    $(this).stickyTableHeaders({scrollableArea: scrollArea, leftOffset: scrollArea})
+"use strict";
+
+var ListView = require("web.ListView");
+
+ListView.include({
+    load_list: function()
+    {
+		var self = this;
+		this._super.apply(this, arguments);
+        $(document).delegate("table.oe_list_content","mouseenter", function(){
+            var scrollArea = self.$el.parents('.oe-view-manager.oe_view_manager_current').find('.oe-view-manager-content .oe-view-manager-view-list')[0];
+            if(scrollArea && $("table.oe_list_content thead").length == 1)
+            {
+                self.$el.find('table.oe_list_content').each(function()
+                {
+                    $(this).stickyTableHeaders({scrollableArea: scrollArea, leftOffset: scrollArea, "fixedOffset": 0.1 })
                 });
             }
-        },
-    });
-};
+        });
+    }
+});
+
+});
