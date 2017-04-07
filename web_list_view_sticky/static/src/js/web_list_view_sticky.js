25dc5c66b1dc1a8d62d8ffd012fb26702b1ceab5
Deepak Kumar Dixit <deepak@ksolves.com>
2017-04-07 18:37:13 +0530

    Alignment fix

--- web_list_view_sticky/static/src/js/web_list_view_sticky.js
+++ web_list_view_sticky/static/src/js/web_list_view_sticky.js
@@ -1,25 +1,34 @@
 odoo.define('web_list_view_sticky.ListView',function(require){
 
-"use strict";
+  "use strict";
 
-var ListView = require("web.ListView");
+  var ListView = require("web.ListView");
 
-ListView.include({
+  ListView.include({
     load_list: function()
     {
 		var self = this;
 		this._super.apply(this, arguments);
-        $(document).delegate("table.oe_list_content","mouseenter", function(){
-            var scrollArea = self.$el.parents('.oe-view-manager.oe_view_manager_current').find('.oe-view-manager-content .oe-view-manager-view-list')[0];
-            if(scrollArea && $("table.oe_list_content thead").length == 1)
+    $(document).delegate("table.oe_list_content","mouseenter", function(){
+      var scrollArea = 
+        self.$el.parents(
+          '.oe-view-manager.oe_view_manager_current'
+        ).find('.oe-view-manager-content .oe-view-manager-view-list')[0];
+      if(scrollArea && $("table.oe_list_content thead").length == 1)
+      {
+        self.$el.find('table.oe_list_content').each(function()
+        {
+          $(this).stickyTableHeaders(
             {
-                self.$el.find('table.oe_list_content').each(function()
-                {
-                    $(this).stickyTableHeaders({scrollableArea: scrollArea, leftOffset: scrollArea, "fixedOffset": 0.1 })
-                });
+              scrollableArea: scrollArea, 
+              leftOffset: scrollArea, 
+              "fixedOffset": 0.1 
             }
+          )
         });
+      }
+    });
     }
-});
+  });
 
 });
